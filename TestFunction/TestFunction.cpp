6da392abdae6e4170b75fbc6af5a7a7b64037f3a
kamirusubi <michaileremeev2005@gmail.com>
2025-04-29 10:51:20 +0300

    Создан проект для тестирования

---
+++ TestFunction/TestFunction.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../eremeev_kinpo/error.h"
+#include "../eremeev_kinpo/exprNode.h"
+#include "../eremeev_kinpo/header.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestFunctions
+{
+	TEST_CLASS(TestFunctions1111)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+	TEST_CLASS(TestFunctions2222)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
