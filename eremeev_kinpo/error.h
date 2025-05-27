16f5c054d340de388261ce12309009e857e2e953
kamirusubi <michaileremeev2005@gmail.com>
2025-05-27 14:50:19 +0300

    Удалено лишнее поле outputFilePath в клаccе Error

--- eremeev_kinpo/error.h
+++ eremeev_kinpo/error.h
@@ -7,9 +7,8 @@ class Error {
 public:
     ErrorType type; //тип ошибки
     std::string errorInputFilePath; //поле для хранения пути, откуда не удалось прочитать файл
-    std::string errorOutputFilePath; //поле для хранения пути, куда не удалось записать файл
     std::string strWithError; // строка, содержащая ошибку
-    int position;
+    int position; // позиция ошибки
 
     //Конструктор для входных параметров с типом ошибки и ее описанием
     Error(ErrorType _type, std::string _strWithError, int _position) :
